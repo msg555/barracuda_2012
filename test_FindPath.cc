d0efea39910fe9e3ac7cc32ae7df268b4a22ed24
mpiuser <mpiuser@ahiru02.(none)>
2012-10-05 21:58:31 -0400

    .

--- test_FindPath.cc
+++ test_FindPath.cc
@@ -57,6 +57,25 @@ void test_3_by_3_vertical() {
   assert(foo.Search() == 1);
 }
 
+void test_3_by_3_no_path() {
+  vector<vector<int > > a;
+  int n = 3;
+  for (int i = 0; i < n; ++i) { 
+    vector<int> s;
+    for (int j = 0; j < n; ++j)
+      s.push_back(0);
+    a.push_back(s);
+  }
+  a[0][0] = -1;
+  a[0][1] = -1;
+  a[0][2] = -1;
+  a[1][1] = -1;
+  a[2][2] = -1;
+  a[1][2] = -2;
+  FindPath foo(a, 0, true);
+  assert(foo.Search() == -1);
+}
+
 void test_4_by_4() {
   vector<vector<int > > a;
   int n = 4;
@@ -104,12 +123,14 @@ void test_4_by_4_vertical() {
 }
 
 int main() {
-  
+   
   test_2_by_2();
   test_3_by_3();
   test_4_by_4();
   
   test_3_by_3_vertical();
   test_4_by_4_vertical();
+  
+  test_3_by_3_no_path();
   return -1;
 }
